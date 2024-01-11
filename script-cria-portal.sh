7314f8574b0d748556a1a627f6c2d3f7cb1e8fd6
klaybson <klaybson@gmail.com>
2024-01-11 18:40:56 +0000

    correcao de front

--- script-cria-portal.sh
+++ script-cria-portal.sh
@@ -2,15 +2,16 @@
 
 # Solicita o nome do portal do argumento passado
 portal="$1"
+ID=$("docker ps -q -f name=$portal ")
 
 # Verifica se o arquivo que armazena a porta já existe no diretório de controle
-if [ ! -f "/srv/scripts/script-cria-portal/devops/porta-controle/port_$portal" ]; then
+if [ ! -f "/var/www/html/devops/porta-controle/port_$portal" ]; then
   # Se não existe, gera uma porta aleatória
   porta=$(shuf -i 8000-8999 -n 1)
-  echo $porta > "/srv/scripts/script-cria-portal/devops/porta-controle/port_$portal"
+  echo $porta > "/var/www/html/devops/porta-controle/port_$portal"
 else
   # Se o arquivo existe, lê a porta armazenada
-  porta=$(cat "/srv/scripts/script-cria-portal/devops/porta-controle/port_$portal")
+  porta=$(cat "/var/www/html/devops/porta-controle/port_$portal")
 fi
 
 # Criar diretório do volume
@@ -31,4 +32,11 @@ echo "Tentativa de execução do Docker" >> /tmp/docker_script_log.txt
   portais-tjba:1 2>&1 >> /tmp/docker_script_log.txt
 
 echo "Portal $portal criado"
+echo ""
 echo "URL: http://srvslabl002.tjba.jus.br:$porta "
+echo "Bind: /var/www/html/$portal "
+echo "ID: $ID"
+echo ""
+echo "Apache: $("docker exec $ID apache -v ") "
+echo "PHP: $("docker exec $ID php -v ") "
+
