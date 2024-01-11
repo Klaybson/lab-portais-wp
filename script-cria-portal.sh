8d9857ab2a2f13d2764bbc659d54a90a2e0341b4
klaybson <klaybson@gmail.com>
2024-01-11 17:48:35 +0000

    correcao gerencia de portas

--- script-cria-portal.sh
+++ script-cria-portal.sh
@@ -4,13 +4,13 @@
 portal="$1"
 
 # Verifica se o arquivo que armazena a porta já existe no diretório de controle
-if [ ! -f "/var/www/html/devops/porta-controle/port_$portal" ]; then
+if [ ! -f "/srv/scripts/script-cria-portal/devops/porta-controle/port_$portal" ]; then
   # Se não existe, gera uma porta aleatória
   porta=$(shuf -i 8000-8999 -n 1)
-  echo $porta > "/var/www/html/devops/porta-controle/port_$portal"
+  echo $porta > "/srv/scripts/script-cria-portal/devops/porta-controle/port_$portal"
 else
   # Se o arquivo existe, lê a porta armazenada
-  porta=$(cat "/var/www/html/devops/porta-controle/port_$portal")
+  porta=$(cat "/srv/scripts/script-cria-portal/devops/porta-controle/port_$portal")
 fi
 
 # Criar diretório do volume
